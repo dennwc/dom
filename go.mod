2ecd0979c082358a85ac51ea8d6e87282eb32e45
Denys Smirnov <denys@sourced.tech>
2018-11-16 23:32:48 +0200

    jstest: support testing in headless chrome browser

--- go.mod
+++ go.mod
@@ -7,8 +7,11 @@ require (
 	github.com/Microsoft/go-winio v0.4.11 // indirect
 	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/cenkalti/backoff v2.0.0+incompatible // indirect
+	github.com/chromedp/cdproto v0.0.0-20181114223838-97b2d384bb9e
+	github.com/chromedp/chromedp v0.1.2
 	github.com/containerd/continuity v0.0.0-20181027224239-bea7585dbfac // indirect
 	github.com/dennwc/testproxy v1.0.1
+	github.com/disintegration/imaging v1.5.0 // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.3.3 // indirect
 	github.com/google/go-cmp v0.2.0 // indirect
@@ -22,7 +25,10 @@ require (
 	github.com/pkg/errors v0.8.0 // indirect
 	github.com/sirupsen/logrus v1.2.0 // indirect
 	github.com/stretchr/testify v1.2.2
+	golang.org/x/image v0.0.0-20181115061151-d9c2484c482e // indirect
 	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a // indirect
 	golang.org/x/sys v0.0.0-20181107165924-66b7b1311ac8 // indirect
 	gotest.tools v2.2.0+incompatible // indirect
 )
+
+replace github.com/chromedp/chromedp => github.com/dennwc/chromedp v0.1.3-0.20181116212057-ac5b86a6b983
---
+++ js/jstest/chrome.go
@@ -0,0 +1,304 @@
+//+build !js
+
+package jstest
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"log"
+	"math/rand"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	clog "github.com/chromedp/cdproto/log"
+	"github.com/chromedp/cdproto/runtime"
+	"github.com/chromedp/chromedp"
+	"github.com/chromedp/chromedp/client"
+	"github.com/dennwc/dom/internal/goenv"
+	"github.com/dennwc/testproxy"
+	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/docker"
+	"github.com/stretchr/testify/require"
+)
+
+const chromeImage = "chromedp/headless-shell:70.0.3526.1"
+
+var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// RunTestChrome compiles all tests in the current working directory for WASM+JS, and runs them in a headless Chrome
+// browser using Docker. It will stream test results back to t.
+//
+// Optionally, the default HTTP handler can be set to serve additional content to the the script.
+//
+// The caller should specify the "!js" build tag, while all JS tests in the package should include "js" build tag.
+func RunTestChrome(t *testing.T, def http.Handler) {
+	testFile := buildTestJS(t)
+	defer os.Remove(testFile)
+
+	addr, closer := runChromeInDocker(t)
+	defer closer()
+
+	rn := newChromeRunner(addr, testFile, def)
+	testproxy.RunAndReplay(t, rn)
+}
+
+func newChromeRunner(addr, testfile string, def http.Handler) testproxy.Runner {
+	c := client.New(client.URL("http://" + addr + "/json"))
+	return &chromeRunner{
+		c: c, bin: testfile, def: def,
+	}
+}
+
+type chromeRunner struct {
+	c   *client.Client
+	bin string
+	def http.Handler
+}
+
+func (rn *chromeRunner) RunAndWait(stdout, stderr io.Writer) error {
+	goroot := goenv.GOROOT()
+	port := strconv.Itoa(rnd.Intn(10000) + 10000)
+
+	srv := &http.Server{
+		Addr: ":" + port,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			path := strings.Trim(r.URL.Path, "/")
+			switch path {
+			case "", "index.html":
+				w.Write([]byte(indexHTML))
+			case "wasm_exec.js":
+				http.ServeFile(w, r, filepath.Join(goroot, wasmDir, wasmExecJS))
+			case "test.wasm":
+				http.ServeFile(w, r, rn.bin)
+			default:
+				if rn.def != nil {
+					rn.def.ServeHTTP(w, r)
+				} else {
+					w.WriteHeader(http.StatusNotFound)
+				}
+			}
+		}),
+	}
+	defer srv.Close()
+
+	errc := make(chan error, 1)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			errc <- err
+		}
+	}()
+
+	// TODO: discover Docker host IP
+	surl := "http://172.17.0.1:" + port
+
+	ctx := context.TODO()
+	c := rn.c
+	tg, err := c.NewPageTargetWithURL(ctx, "about:blank")
+	if err != nil {
+		return err
+	}
+	defer c.CloseTarget(ctx, tg)
+
+	logf := func(format string, args ...interface{}) {
+		log.Printf(format, args...)
+	}
+	noop := func(format string, args ...interface{}) {}
+	_ = noop
+
+	h, err := chromedp.NewTargetHandler(tg, logf, noop, logf)
+	if err != nil {
+		return err
+	}
+
+	h.OnEvent(func(ev interface{}) {
+		switch ev := ev.(type) {
+		case *runtime.EventConsoleAPICalled:
+			if ev.Type != "log" || len(ev.Args) != 1 {
+				return
+			}
+			var line string
+			if err := json.Unmarshal(ev.Args[0].Value, &line); err != nil {
+				select {
+				case errc <- err:
+				default:
+				}
+				return
+			}
+			line += "\n"
+			stdout.Write([]byte(line))
+		case *clog.EventEntryAdded:
+			stderr.Write([]byte(ev.Entry.Text + "\n"))
+		}
+	})
+
+	err = h.Run(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = chromedp.Navigate(surl).Do(ctx, h)
+	if err != nil {
+		return err
+	}
+
+	err = chromedp.WaitEnabled("#runButton").Do(ctx, h)
+	if err != nil {
+		return err
+	}
+
+	// TODO: better wait logic
+	var res interface{}
+	err = chromedp.Evaluate("run().then(() => {done = true}).catch(() => {done = true})", &res).Do(ctx, h)
+	if err != nil {
+		return err
+	}
+
+	var done bool
+	err = chromedp.Evaluate("done", &done).Do(ctx, h)
+	if err != nil {
+		return err
+	}
+	ticker := time.NewTicker(time.Second / 3)
+	defer ticker.Stop()
+	for !done {
+		err = chromedp.Evaluate("done", &done).Do(ctx, h)
+		if err != nil {
+			return err
+		}
+		select {
+		case <-ticker.C:
+		case err = <-errc:
+			return err
+		}
+	}
+	return nil
+}
+
+func runChromeInDocker(t testing.TB) (string, func()) {
+	p, err := dockertest.NewPool("")
+	require.NoError(t, err)
+	cli := p.Client
+
+	if !pullIfNotExists(cli, chromeImage) {
+		t.SkipNow()
+		return "", func() {}
+	}
+
+	c, err := cli.CreateContainer(docker.CreateContainerOptions{
+		Config: &docker.Config{
+			Image: chromeImage,
+		},
+	})
+	require.NoError(t, err)
+
+	cw, err := cli.AttachToContainerNonBlocking(docker.AttachToContainerOptions{
+		Container: c.ID,
+		//OutputStream: os.Stderr,
+		//ErrorStream: os.Stderr,
+		Stdout: false, Stderr: false,
+		Logs: true, Stream: true,
+	})
+	if err != nil {
+		cli.RemoveContainer(docker.RemoveContainerOptions{
+			ID: c.ID, RemoveVolumes: true, Force: true,
+		})
+		require.NoError(t, err)
+	}
+
+	remove := func() {
+		cw.Close()
+		cli.RemoveContainer(docker.RemoveContainerOptions{
+			ID: c.ID, RemoveVolumes: true, Force: true,
+		})
+	}
+	t.Log("running in Chrome in Docker")
+
+	err = cli.StartContainer(c.ID, nil)
+	if err != nil {
+		remove()
+		require.NoError(t, err)
+	}
+
+	info, err := cli.InspectContainer(c.ID)
+	if err != nil {
+		remove()
+		require.NoError(t, err)
+	}
+	addr := info.NetworkSettings.IPAddress + ":9222"
+	if !waitPort(addr) {
+		remove()
+		require.Fail(t, "timeout")
+	}
+	return addr, remove
+}
+
+func waitPort(addr string) bool {
+	for i := 0; i < 3; i++ {
+		c, err := net.Dial("tcp", addr)
+		if err == nil {
+			c.Close()
+			return true
+		}
+		time.Sleep(time.Second * 3)
+	}
+	return false
+}
+
+const indexHTML = `<!doctype html>
+<!--
+Copyright 2018 The Go Authors. All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+-->
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Go wasm</title>
+</head>
+
+<body>
+	<!--
+	Add the following polyfill for Microsoft Edge 17/18 support:
+	<script src="https://cdn.jsdelivr.net/npm/text-encoding@0.7.0/lib/encoding.min.js"></script>
+	(see https://caniuse.com/#feat=textencoder)
+	-->
+	<script src="wasm_exec.js"></script>
+	<script>
+		if (!WebAssembly.instantiateStreaming) { // polyfill
+			WebAssembly.instantiateStreaming = async (resp, importObject) => {
+				const source = await (await resp).arrayBuffer();
+				return await WebAssembly.instantiate(source, importObject);
+			};
+		}
+
+		const go = new Go();
+		go.argv.push('-test.v');
+		let mod, inst;
+		WebAssembly.instantiateStreaming(fetch("test.wasm"), go.importObject).then((result) => {
+			mod = result.module;
+			inst = result.instance;
+			document.getElementById("runButton").disabled = false;
+		}).catch((err) => {
+			console.error(err);
+		});
+
+		async function run() {
+			console.clear();
+			await go.run(inst);
+		}
+		let done = false;
+	</script>
+
+	<button onClick="run();" id="runButton" disabled>Run</button>
+</body>
+
+</html>`
