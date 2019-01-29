868e0996e85d42723d838739f9e6323efa876a7c
Denys Smirnov <denis.smirnov.91@gmail.com>
2019-01-29 15:06:45 +0200

    Update node.go
    Co-Authored-By: koodimetsa <make.metsa@gmail.com>

--- node.go
+++ node.go
@@ -109,7 +109,9 @@ func (e *NodeBase) ReplaceChild(n, old Node) Node {
 }
 
 func (e *NodeBase) AttachShadow() Node {
-	m := make(map[string]interface{})
+	m := map[string]interface{}{
+		"mode":"open",
+	}
 	m["mode"] = "open"
 	return AsElement(e.v.Call("attachShadow", js.ValueOf(m)))
 }
