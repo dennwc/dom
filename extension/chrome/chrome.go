26673a78b59fa331e83af0d92e901b49341aa273
Denys Smirnov <denys@sourced.tech>
2019-03-02 14:09:20 +0200

    backport a few changes from dev branch

--- extension/chrome/chrome.go
+++ extension/chrome/chrome.go
@@ -4,8 +4,19 @@ package chrome
 
 import "github.com/dennwc/dom/js"
 
-var chrome = js.Get("chrome")
+var (
+	chrome  = js.Get("chrome")
+	runtime = chrome.Get("runtime")
+)
 
 type WindowID int
 
 const CurrentWindow = WindowID(0)
+
+func lastError() error {
+	e := runtime.Get("lastError")
+	if !e.Valid() {
+		return nil
+	}
+	return js.Error{Value: e.JSValue()}
+}
--- extension/chrome/tabs.go
+++ extension/chrome/tabs.go
@@ -4,14 +4,23 @@ package chrome
 
 import "github.com/dennwc/dom/js"
 
+type TabOptions struct {
+	Active bool
+}
+
 type AllTabs interface {
 	js.Wrapper
 	GetCurrent() Tab
 	GetSelected(window WindowID) Tab
 	GetAllInWindow(window WindowID) []Tab
+	Create(url string, opt *TabOptions) Tab
 }
 
 func Tabs() AllTabs {
+	return tabs()
+}
+
+func tabs() jsTabs {
 	return jsTabs{v: chrome.Get("tabs")}
 }
 
@@ -33,6 +42,7 @@ func (t jsTabs) callAsync(name string, args ...interface{}) js.Value {
 	t.v.Call(name, args...)
 	return <-ch
 }
+
 func (t jsTabs) GetCurrent() Tab {
 	v := t.callAsync("getCurrent")
 	if !v.Valid() {
@@ -40,6 +50,7 @@ func (t jsTabs) GetCurrent() Tab {
 	}
 	return jsTab{v}
 }
+
 func (t jsTabs) GetSelected(window WindowID) Tab {
 	var win interface{}
 	if window != 0 {
@@ -51,6 +62,7 @@ func (t jsTabs) GetSelected(window WindowID) Tab {
 	}
 	return jsTab{v}
 }
+
 func (t jsTabs) GetAllInWindow(window WindowID) []Tab {
 	var win interface{}
 	if window != 0 {
@@ -63,6 +75,17 @@ func (t jsTabs) GetAllInWindow(window WindowID) []Tab {
 		tabs = append(tabs, jsTab{v})
 	}
 	return tabs
+}
+
+func (t jsTabs) Create(url string, opt *TabOptions) Tab {
+	obj := js.Obj{
+		"url": url,
+	}
+	if opt != nil {
+		obj["active"] = opt.Active
+	}
+	v := t.callAsync("create", obj)
+	return jsTab{v}
 }
 
 type Tab interface {
@@ -78,6 +101,13 @@ type Tab interface {
 	URL() string
 	Title() string
 	Size() (w, h int)
+
+	ExecuteFile(path string) (js.Value, error)
+	ExecuteCode(code string) (js.Value, error)
+}
+
+func AsTab(v js.Value) Tab {
+	return jsTab{v: v}
 }
 
 type jsTab struct {
@@ -132,4 +162,47 @@ func (t jsTab) Size() (w, h int) {
 	w = t.v.Get("width").Int()
 	h = t.v.Get("height").Int()
 	return
+}
+
+func (t jsTab) executeScript(obj interface{}) ([]js.Value, error) {
+	id := t.ID()
+	ch := make(chan []js.Value, 1)
+	errc := make(chan error, 1)
+	cb := js.NewCallback(func(args []js.Value) {
+		err := lastError()
+		if err != nil {
+			errc <- err
+			return
+		}
+		v := args[0]
+		if !v.Valid() {
+			ch <- nil
+		} else {
+			ch <- v.Slice()
+		}
+	})
+	defer cb.Release()
+	tabs().v.Call("executeScript", id, obj, cb)
+	select {
+	case err := <-errc:
+		return nil, err
+	case res := <-ch:
+		return res, nil
+	}
+}
+
+func (t jsTab) executeScriptOne(obj interface{}) (js.Value, error) {
+	res, err := t.executeScript(obj)
+	if err != nil || len(res) == 0 {
+		return js.Value{}, err
+	}
+	return res[0], nil
+}
+
+func (t jsTab) ExecuteFile(path string) (js.Value, error) {
+	return t.executeScriptOne(js.Obj{"file": path})
+}
+
+func (t jsTab) ExecuteCode(code string) (js.Value, error) {
+	return t.executeScriptOne(js.Obj{"code": code})
 }
