5686e527bc0291ac59fe085e04f32d29a4494cfe
Denys Smirnov <denys@sourced.tech>
2019-03-02 14:09:20 +0200

    backport a few changes from dev branch

--- examples/grpc-over-ws/run.sh
+++ examples/grpc-over-ws/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 WASM_FILES="$(go env GOROOT)/misc/wasm"
-GOOS=js GOARCH=wasm go build -o client.wasm ./client.go
+GOOS=js GOARCH=wasm go build -o client.wasm ./client.go || exit 1
 cp ${WASM_FILES}/wasm_exec.js ./
 go run ./server.go
